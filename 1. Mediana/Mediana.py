5828bf1f57c08b730f8708cd0adeb18de275f91b
nedved942 <pawelkoziol77@wp.pl>
2024-05-06 20:51:42 +0200

    Gotowe rozwiazanie

--- 1. Mediana/Mediana.py
+++ 1. Mediana/Mediana.py
@@ -2,41 +2,48 @@ from statistics import median
 from datetime import datetime
 
 
-example_date_string = "2023-03"
-print(example_date_string, type(example_date_string))
+expenses = {
+    "2023-01": {
+        "01": {
+            "food": [22.11, 43, 11.72, 2.2, 36.29, 2.5, 19],
+            "fuel": [210.22]
+        },
+        "09": {
+            "food": [11.9],
+            "fuel": [190.22]
+        }
+    },
+    "2023-03": {
+        "07": {
+            "food": [20, 11.9, 30.20, 11.9]
+        },
+        "04": {
+            "food": [10.20, 11.50, 2.5],
+            "fuel": []
+        }
+    },
+    "2023-04": {}
+}
 
-example_date_datetime = datetime.strptime(example_date_string, "%Y-%m")
-print(example_date_datetime, type(example_date_datetime))
 
-example_date_string = example_date_datetime.strftime("%Y-%m")
-print(example_date_string, type(example_date_string))
+def solution(dict_with_expenses):
+    result = None
+    end_list = []
+    for year_and_month, days_data in dict_with_expenses.items():
+        year_and_month_datetime = datetime.strptime(year_and_month, "%Y-%m")
+        number_day_of_first_sunday = 1 + (6 - year_and_month_datetime.weekday())
+        for day, lists_of_values in days_data.items():
+            current_day = int(day)
+            if current_day <= number_day_of_first_sunday:
+                current_list = []
+                food_values = lists_of_values.get("food", [])
+                fuel_values = lists_of_values.get("fuel", [])
+                current_list.extend(food_values)
+                current_list.extend(fuel_values)
+                end_list.extend(current_list)
+    print(end_list)
+    result = median(end_list)
+    return result
 
-example_day = "04"
 
-
-def solution(example_month_and_year, example_current_day):
-    list_of_medians = []
-    number_day_of_first_sunday = 1 + (6 - example_month_and_year.weekday())
-    print(number_day_of_first_sunday)
-    example_current_day = int(example_current_day)
-    if example_current_day <= number_day_of_first_sunday:
-        list_of_values = [1, 2, 3, 4]
-        current_median = median(list_of_values)
-        list_of_medians.append(current_median)
-        print(list_of_medians)
-    pass
-
-
-solution(example_date_datetime, example_day)
-
-"""
-def check_is_day_to_count(date):
-    format_date = "%Y-%m-%d"
-    date = datetime.strptime(date, format_date)
-    print(date)
-    # for day in date:
-    #     print(day)
-    pass
-    
-check_is_day_to_count(example_date)    
-"""
+print(solution(expenses))
